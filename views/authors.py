57632134775cb38e031a489d73d1d22ab67cf5c2
root <root@103.(none)>
2017-09-18 11:12:35 -0600

    stoplight report backend

--- views/authors.py
+++ views/authors.py
@@ -3,6 +3,8 @@ from pyramid.response import Response
 from pyramid.view import view_config
 from ..models import DBSession
 from ..models.datasets import Dataset
+from ..models.categories import Category
+from sqlalchemy import func
 
 @view_config(route_name='authors')
 def authors(request):
@@ -11,6 +13,22 @@ def authors(request):
     resp = {}
     rslts = []
     rslts = [{'author': r.author} for r in d]
+    resp.update({"results": rslts})
+    response = Response(json.dumps(resp))
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.content_type="application/json"
+    return response
+
+@view_config(route_name='datasetsreport')
+def datasetsreport(request):
+    after=request.params.get('after') if 'after' in request.params else '1050-01-01'
+    before=request.params.get('before') if 'before' in request.params else '9850-01-01'
+    excludedtheme=request.params.get('excludetheme') if 'excludetheme' in request.params else 'lol'
+    app = request.matchdict['app']
+    d = DBSession.query(Dataset.author, Category.theme, func.count().label('c')).filter(Category.theme!=excludedtheme,Dataset.is_available==True,Dataset.dateadded>=after,Dataset.dateadded<=before,*["apps_cache @> ARRAY['%s']" % (app)]).group_by(Dataset.author, Category.theme).distinct().join(Dataset.categories).order_by('c DESC')
+    resp = {}
+    rslts = []
+    rslts = [{'author': r.author,'count':r.c, 'theme':r.theme} for r in d]
     resp.update({"results": rslts})
     response = Response(json.dumps(resp))
     response.headers['Access-Control-Allow-Origin'] = '*'
